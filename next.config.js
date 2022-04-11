d7bb8b6de4ec5d486ab9e908edf0e940ca97efd0
AnasMA94 <anas@labelideas.co>
2022-04-11 12:44:58 +0700

    Deploy config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
 
 module.exports = {
-  reactStrictMode:true,
-  env : {
+  reactStrictMode: true,
+  env: {
     API_ENDPOINT: process.env.API_ENDPOINT
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 }
